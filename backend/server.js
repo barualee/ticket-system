923fb3d2cb74ba191192c0d329298bb89921d642
barualee <barua.nishant97@gmail.com>
2024-01-30 12:54:24 -0500

    second

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("server...");
+const express = require("express")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT
+
+const app = express()
+app.get("/", (req,res) => {
+    res.status(201).json({'message':"Welcome to support desk"})
+})
+app.listen(PORT,() => console.log(`Server started on ${PORT}`))
