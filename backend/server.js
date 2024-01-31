b9f01fe2a5b5201b4efe8f5a200e26caabf2f748
barualee <barua.nishant97@gmail.com>
2024-01-31 08:06:56 -0500

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const {errorHandler} = require('./Middleware/errorMiddleware')
 const PORT = process.env.PORT
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.get("/", (req,res) => {
     res.status(201).json({'message':"Welcome to support desk"})
 })
+app.use('/api/users', require('./Routes/userRoutes'))
+app.use(errorHandler)
 app.listen(PORT,() => console.log(`Server started on ${PORT}`))
